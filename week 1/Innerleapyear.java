a6d6269480d6bad66ad9649168fbb8594052d2e1
aniket2040 <aniket8873kumar@gmail.com>
2024-12-25 21:06:28 +0530

    first commit

---
+++ week 1/Innerleapyear.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Innerleapyear {
+public static void main(String[] args) {
+    int a;
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter year");
+    a= in.nextInt();
+    if (a%4==0){
+        System.out.println("Entered year is a leapyear");
+    }else{
+        System.out.println("Entered year is not a leapyear");
+    }
+}
+}
