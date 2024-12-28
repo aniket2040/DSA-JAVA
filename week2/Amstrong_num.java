8e79fa0a26bb9a452f2e9ff6781d18822941f216
aniket2040 <aniket8873kumar@gmail.com>
2024-12-28 19:43:15 +0530

    13th commit

---
+++ week2/Amstrong_num.java
@@ -0,0 +1,31 @@
+package week2;
+import java.util.Scanner;
+public class Amstrong_num{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the lower number");
+        int lower = sc.nextInt();
+        System.out.println("Enter the upper number");
+        int upper = sc.nextInt();
+        System.out.println("Armstrong number between" +lower+"and"+upper+"are:");
+        for(int i=lower;i<=upper;i++){
+            if (isArmstrong(i)) {
+                System.out.println(i);
+                
+            }
+        }
+    
+    }
+
+public static boolean isArmstrong(int number){
+    int original = number;
+    int sum = 0;
+    int digit = String.valueOf(number).length();
+    while (number != 0) {
+        int remainder = number % 10;
+        sum = sum + (int) Math.pow(remainder, digit);
+        number = number / 10;
+        }
+        return original == sum;
+    }
+}
